d0cf241a28f1a96a49461d92452c5908f58173fd
Matthew632 <matthew.hebb@hotmail.co.uk>
2019-04-23 10:33:16 +0100

    adding map

--- TryoutProject/App.js
+++ TryoutProject/App.js
@@ -9,42 +9,43 @@ const deltas = {
 
 export default class Map extends React.Component {
   state = {
-    region: null,
-    coffeeShops: []
+    region: {
+      latitude: 55.953251,
+      longitude: -3.188267,
+      ...deltas
+    },
+    restaurants: []
   };
 
-  componentWillMount() {
+  onMapReady = () => {
     this.getLocationAsync();
-  }
+  };
 
   getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== "granted") {
       this.setState({
         errorMessage:
-          "Unable to access location: Defaulted to Manchester location"
+          "Unable to access location: Defaulted to Edinburgh location"
       });
     }
 
     let location = await Location.getCurrentPositionAsync({});
-    const region = {
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
+    const gotRegion = {
+      latitude: 40.73061,
+      longitude: -73.935242,
       ...deltas
     };
-    await this.setState({ region });
+    await this.setState({ region: gotRegion });
   };
 
   render() {
+    console.log(this.state.region);
     return (
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 53.4808,
-          longitude: -2.2426,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
+        initialRegion={this.state.region}
+        onMapReady={this.onMapReady}
       />
     );
   }
